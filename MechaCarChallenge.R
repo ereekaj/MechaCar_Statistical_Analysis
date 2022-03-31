160ea309be0e19a8e30bc1ee296e5e8da55cdbd4
ereekaj <52435827+ereekaj@users.noreply.github.com>
2022-03-31 15:23:00 -0600

    adding screenshots and code del 2

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -5,3 +5,12 @@ head(mecha_car_df)
 
 lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data = mecha_car_df)
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data = mecha_car_df))
+
+suspension_coil <- read.csv(file="Resources/Suspension_Coil.csv")
+head(suspension_coil)
+
+total_summary <- suspension_coil %>% summarize(Mean = mean(PSI), Median = median(PSI), Variance = var(PSI), SD =  sd(PSI), .groups = 'keep')
+total_summary
+
+lot_summary <- suspension_coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean = mean(PSI), Median = median(PSI), Variance = var(PSI), SD =  sd(PSI), .groups = 'keep')
+lot_summary
